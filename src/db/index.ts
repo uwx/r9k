912ef476c80cbe5233903b74b6d0d2497be559ec
uwx <maxine@cybergal.com>
2026-02-22 04:38:56 +0100

    use node:sqlite

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,13 +1,45 @@
-import SqliteDb from 'better-sqlite3'
 import { Kysely, Migrator, SqliteDialect } from 'kysely'
 import { DatabaseSchema } from './schema'
 import { migrationProvider } from './migrations'
 
+import { DatabaseSync } from 'node:sqlite'
+import { buildQueryFn, GenericSqliteDialect, parseBigInt } from 'kysely-generic-sqlite'
+import type { IGenericSqlite } from 'kysely-generic-sqlite'
+
+function createSqliteExecutor(db: DatabaseSync): IGenericSqlite<DatabaseSync> {
+  const getStmt = (sql: string) => {
+    const stmt = db.prepare(sql)
+    stmt.setReadBigInts(true)
+    return stmt
+  }
+
+  return {
+    db,
+    query: buildQueryFn({
+      all: (sql, parameters = []) => getStmt(sql).all(...parameters),
+      run: (sql, parameters = []) => {
+        const { changes, lastInsertRowid } = getStmt(sql).run(...parameters)
+        return {
+          insertId: parseBigInt(lastInsertRowid),
+          numAffectedRows: parseBigInt(changes),
+        }
+      },
+    }),
+    close: () => db.close(),
+    iterator: (isSelect, sql, parameters = []) => {
+      if (!isSelect) {
+        throw new Error('Only support select in stream()')
+      }
+      return getStmt(sql).iterate(...parameters) as any
+    },
+  }
+}
+
 export const createDb = (location: string): Database => {
   return new Kysely<DatabaseSchema>({
-    dialect: new SqliteDialect({
-      database: new SqliteDb(location),
-    }),
+    dialect: new GenericSqliteDialect(
+      () => createSqliteExecutor(new DatabaseSync(location)),
+    ),
   })
 }
 
